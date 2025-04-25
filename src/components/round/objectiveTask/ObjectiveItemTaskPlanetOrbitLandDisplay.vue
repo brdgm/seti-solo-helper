0afc5fed266c51350a3edacb98b9b557e6912a0d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-04-26 00:18:35 +0200

    improve icon display

--- src/components/round/objectiveTask/ObjectiveItemTaskPlanetOrbitLandDisplay.vue
+++ src/components/round/objectiveTask/ObjectiveItemTaskPlanetOrbitLandDisplay.vue
@@ -4,8 +4,11 @@
     <span>:</span>
   </div>
   <template v-for="(planet,index) of task.planets" :key="planet">
-    <template v-if="index > 0">/</template>    
-    <AppIcon type="planet" :name="planet" class="planetIcon"/>
+    <div v-if="index > 0" class="divider">/</div>
+    <div class="planet">
+      <AppIcon type="planet" :name="planet" class="planetIcon"/>
+      <div class="name">{{t(`planet.${planet}`).substring(0,3)}}</div>
+    </div>
   </template>
 </template>
 
@@ -13,11 +16,16 @@
 import { defineComponent, PropType } from 'vue'
 import AppIcon from '@/components/structure/AppIcon.vue'
 import { ObjectiveItemTaskPlanetOrbitLand } from '@/services/Objective'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'ObjectiveItemTaskPlanetOrbitLandDisplay',
   components: {
     AppIcon
+  },
+  setup() {
+    const { t } = useI18n()
+    return { t }
   },
   props: {
     task: {
@@ -32,7 +40,20 @@ export default defineComponent({
 .icon {
   height: 2rem;
 }
-.planetIcon {
-  height: 1.25rem;
+.planet {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .planetIcon {
+    height: 1.25rem;
+    object-fit: contain;
+  }
+  .name {
+    font-size: 0.5rem;
+    margin-top: -0.1rem;
+  }
+}
+.divider {
+  margin-top: -0.5rem;
 }
 </style>
