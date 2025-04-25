ef3c3b4c21319badf3bdf83e9a0e75044e1cb99f
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-04-25 21:00:50 +0200

    objectives

---
+++ src/services/enum/ObjectiveTask.ts
@@ -0,0 +1,21 @@
+/**
+ * Objective Task
+ */
+enum ObjectiveTask {
+  PUBLICITY = 'publicity',
+  VICTORY_POINTS = 'victory-points',
+  DATA_TOKENS = 'data-tokens',
+  PROBE_VISIT_ASTEROIDS = 'probe-visit-asteroids',
+  PROBE_VISIT_COMET = 'probe-visit-comet',
+  ACTION_PROBE_LAND = 'action-probe-land',
+  ACTION_PROBE_ORBIT = 'action-probe-orbit',
+  ACTION_LAUNCH = 'action-launch',
+  ACTION_TELESCOPE = 'action-telescope',
+  SECTOR_WIN = 'sector-win',
+  LIFE_TRACE = 'life-trace',
+  PLANET_ORBIT_LAND = 'planet-orbit-land',
+  TECH_TYPE = 'tech-type',
+  MISSION_COMPLETE = 'complete-mission',
+  CARD_PLAY_3_CREDITS = 'card-play-3-credits'
+}
+export default ObjectiveTask
---
+++ tests/unit/services/Objectives.spec.ts
@@ -0,0 +1,18 @@
+import ObjectiveLevel from '@/services/enum/ObjectiveLevel'
+import Objectives from '@/services/Objectives'
+import { expect } from 'chai'
+
+describe('services/Objectives', () => {
+  it('get', () => {
+    const objective = Objectives.get(101)
+
+    expect(objective).not.undefined
+    expect(objective?.id).to.eq(101)
+  })
+
+  it('getAll', () => {
+    expect(Objectives.getAll(ObjectiveLevel.LEVEL_1).length).to.eq(4)
+    expect(Objectives.getAll(ObjectiveLevel.LEVEL_2).length).to.eq(11)
+    expect(Objectives.getAll(ObjectiveLevel.LEVEL_3).length).to.eq(9)
+  })
+})
