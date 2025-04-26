d006303848fbfc65cd44a20f6c3cf541a0390818
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-04-26 09:48:28 +0200

    round end

--- src/views/RoundEnd.vue
+++ src/views/RoundEnd.vue
@@ -1,28 +1,23 @@
 <template>
   <SideBar :navigationState="navigationState"/>
+  <ObjectivesTopBar :navigationState="navigationState"/>
   <h1>{{t('roundEnd.title')}}</h1>
 
   <div class="instructions">
     <ol class="mt-4">
       <li v-html="t('roundEnd.gainIncome')"></li>
       <template v-if="hasObjectives">
-        <li v-html="t('roundEnd.objectives.title', {count:expectedCompletedObjectives})"></li>
+        <li v-html="t('roundEnd.objectives.title', {count:expectedCompletedObjectives}, expectedCompletedObjectives)"></li>
         <ul>
-          <li>
-            <span v-html="t('roundEnd.objectives.enterNumberCompleted')"></span>
-            <NumberInput v-model="completedObjectives" class="ms-1"/></li>
-          <template v-if="completedObjectives != undefined">
-            <li v-if="discardObjectives > 0" v-html="t('roundEnd.objectives.discardObjectives', {count:discardObjectives})"></li>
-            <li v-if="botObjectivesProgress > 0" v-html="t('roundEnd.objectives.botObjectiveProgress', {count:botObjectivesProgress})"></li>
-          </template>
+          <li v-html="t('roundEnd.objectives.numberCompleted', {count:completedObjectives}, completedObjectives)"></li>
+          <li v-if="discardObjectives > 0" v-html="t('roundEnd.objectives.discardObjectives', {count:discardObjectives}, discardObjectives)"></li>
+          <li v-if="botObjectivesProgress > 0" v-html="t('roundEnd.objectives.botObjectiveProgress', {count:botObjectivesProgress})"></li>
         </ul>
       </template>
     </ol>
   </div>
 
-  <div v-if="hasObjectives && completedObjectives == undefined" class="alert alert-warning" v-html="t('roundEnd.objectives.objectivesWarning')"></div>
-
-  <button class="btn btn-primary btn-lg mt-4" @click="next()" v-if="!hasObjectives || completedObjectives != undefined">
+  <button class="btn btn-primary btn-lg mt-4" @click="next()">
     {{t('action.next')}}
   </button>
 
@@ -32,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
 import NavigationState from '@/util/NavigationState'
@@ -45,16 +40,17 @@ import getPreviousTurns from '@/util/getPreviousTurns'
 import { MAX_TURN } from '@/util/getTurnOrder'
 import CardDeck from '@/services/CardDeck'
 import DebugInfo from '@/components/round/DebugInfo.vue'
-import NumberInput from '@brdgm/brdgm-commons/src/components/form/NumberInput.vue'
 import DifficultyLevel from '@/services/enum/DifficultyLevel'
+import ObjectiveStack from '@/services/ObjectiveStack'
+import ObjectivesTopBar from '@/components/round/ObjectivesTopBar.vue'
 
 export default defineComponent({
   name: 'RoundEnd',
   components: {
     FooterButtons,
     SideBar,
-    DebugInfo,
-    NumberInput
+    ObjectivesTopBar,
+    DebugInfo
   },
   setup() {
     const { t } = useI18n()
@@ -67,31 +63,23 @@ export default defineComponent({
 
     const routeCalculator = new RouteCalculator({round})
 
-    const completedObjectives = ref(undefined as number|undefined)
+    const previousTurns = getPreviousTurns({state, round, turn:MAX_TURN, player:Player.BOT})
+    const lastTurnBotPersistence = previousTurns[previousTurns.length-1]?.botPersistence
+    const objectiveStack = lastTurnBotPersistence?.objectiveStack ? ObjectiveStack.fromPersistence(lastTurnBotPersistence.objectiveStack) : ObjectiveStack.new(state.setup.difficultyLevel)
 
-    return { t, router, state, navigationState, round, routeCalculator, completedObjectives }
+    // check objective completion / bot progress
+    const hasObjectives = state.setup.difficultyLevel != DifficultyLevel.LEVEL_1
+    const expectedCompletedObjectives = hasObjectives ? round : 0
+    const completedObjectives = objectiveStack.complete.length
+    const discardObjectives = Math.min(expectedCompletedObjectives, completedObjectives)
+    const botObjectivesProgress = (expectedCompletedObjectives - discardObjectives) * 3
+
+    return { t, router, state, navigationState, round, routeCalculator, lastTurnBotPersistence, objectiveStack,
+      hasObjectives, expectedCompletedObjectives, completedObjectives, discardObjectives, botObjectivesProgress }
   },
   computed: {
     backButtonRouteTo() : string {
       return this.routeCalculator.getLastTurnRouteTo(this.state)
-    },
-    hasObjectives() : boolean {
-      return this.state.setup.difficultyLevel != DifficultyLevel.LEVEL_1
-    },
-    expectedCompletedObjectives() : number {
-      return this.round
-    },
-    discardObjectives() : number {
-      if (this.completedObjectives) {
-        return Math.min(this.expectedCompletedObjectives, this.completedObjectives)
-      }
-      return 0
-    },
-    botObjectivesProgress() : number {
-      if (!this.hasObjectives) {
-        return 0
-      }
-      return (this.expectedCompletedObjectives - this.discardObjectives) * 3
     }
   },
   methods: {
@@ -99,23 +87,26 @@ export default defineComponent({
       // prepare next round with new player order
       const startPlayer = this.navigationState.startPlayer == Player.PLAYER ? Player.BOT : Player.PLAYER
       const nextRound = this.round + 1
-      const previousTurns = getPreviousTurns({state:this.state, round:this.round,turn:MAX_TURN,player:Player.BOT})
-      const lastTurn = previousTurns[previousTurns.length-1]
-      if (!lastTurn?.botPersistence) {
+      if (!this.lastTurnBotPersistence) {
         return
       }
-      const cardDeck = CardDeck.fromPersistence(lastTurn.botPersistence.cardDeck)
+      const cardDeck = CardDeck.fromPersistence(this.lastTurnBotPersistence.cardDeck)
       cardDeck.prepareForNextRound()
       const previousTurnResources = this.navigationState.botResources
       const gainResources = this.navigationState.botGainResources
       gainResources.gainProgressSingleStep.value = this.botObjectivesProgress
       const drawAdvancedCards = gainResources.getDrawAdvancedCardCount(previousTurnResources)
       cardDeck.addAdvancedCards(drawAdvancedCards)
+
+      this.objectiveStack.checkCompletedObjectives()
+      this.objectiveStack.discardCompletedObjectives(this.discardObjectives)
+
       const initialBotPersistence : BotPersistence = {
         cardDeck: cardDeck.toPersistence(),
-        objectiveStack: lastTurn.botPersistence.objectiveStack,
+        objectiveStack: this.objectiveStack.toPersistence(),
         resources: gainResources.merge(previousTurnResources)
       }
+
       this.state.storeRound({
         round: nextRound,
         startPlayer,
